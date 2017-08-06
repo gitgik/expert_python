cb91083e76b9f07b71aea8aaa875eb409f67986d
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-08-06 23:49:41 +0300

    add decorators example

---
+++ decorators/dec.py
@@ -0,0 +1,19 @@
+# dec.py
+from time import time
+
+def timer(func):
+    def f(*a, **kwargs):
+        before = time()
+        rv = func(*a, **kwargs)
+        after = time()
+        print('elapsed', after - before)
+        return rv
+    return f
+
+@timer
+def add(x, y=10):
+  return x + y
+
+
+# print ('add(20,30)', add(20, 30))
+print ('add("a" "b")', add("a", "b"))
